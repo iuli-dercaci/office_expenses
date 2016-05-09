77198720c8769668d37a1c121aa0cfa452f1cd84
Iuli Dercaci <iuli.dercaci@gmail.com>
2016-05-10 01:14:14 +0300

    Expense form and related functionality for creating new expense item

---
+++ src/AppBundle/Form/ExpenseType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Choice;
+use Doctrine\ORM\EntityRepository;
+//use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class ExpenseType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('account', EntityType::class, [
+                'class' => 'AppBundle:Account',
+                'choice_label' => 'name',
+                'required' => false,
+            ])
+            ->add('supplier', EntityType::class, [
+                'class' => 'AppBundle:Supplier',
+                'choice_label' => 'name',
+                'required' => false,
+            ])
+            ->add('description')
+            ->add('currency', EntityType::class, [
+                'class' => 'AppBundle:Currency',
+                'choice_label' => 'code',
+                'required' => false,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')->orderBy('c.id', 'ASC');
+                }
+            ])
+            ->add('price')
+            ->add('exchangeRate')
+            ->add('quantity')
+            ->add('total')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Expense'
+        ));
+    }
+}
