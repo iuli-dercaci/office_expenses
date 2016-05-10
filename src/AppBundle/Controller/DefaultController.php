a7b37245f4145feff011cbbb24ce2665a7991c1d
Iuli Dercaci <iuli.dercaci@gmail.com>
2016-05-10 23:08:33 +0300

    Expenses list, templates and css

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,32 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="expenses_list")
      */
-    public function indexAction(Request $request)
+    public function listAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $expenses = $this->getDoctrine()->getRepository('AppBundle:Expense')->findAll();
+        return $this->render('AppBundle:Expense:list.html.twig', compact('expenses'));
     }
+
+    /**
+     * @Route("/create", name="expense_create")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function createAction(Request $request)
+    {
+        $expense = new Expense();
+        $form = $this->createForm(ExpenseType::class, $expense);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($expense);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('expenses_list'));
+        }
+        return $this->render('AppBundle:Expense:create.html.twig', ['form' => $form->createView()]);
+    }
+
 }
